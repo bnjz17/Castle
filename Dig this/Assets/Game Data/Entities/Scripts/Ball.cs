8a24eb66da15d7f321e0080291ecba946a5f64f5
Djedjeska <jeremie.savon@gmail.com>
2020-04-30 23:29:17 +0800

    Anti falling
    new update anti falling

---
+++ Dig this/Assets/Game Data/Entities/Scripts/Ball.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+}
---
+++ Dig this/Assets/Game Data/Entities/Scripts/bowl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bowl : MonoBehaviour
+{
+    public GameObject ring;
+    public bool isActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ring.GetComponent<Renderer>().material.color = new Color32(255, 255, 255, 255);
+        isActive = true;
+
+        GameManager.instance.CheckWin();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ring.GetComponent<Renderer>().material.color = new Color32(156, 236, 128, 255);
+        isActive = false;
+    }
+}
